63d2352dc72f0e62d636fdab08a441b49d46dc93
Timothy Martin-Vigier <tmvigier@emi.u-bordeaux1.fr>
2019-12-06 01:32:27 +0100

    Ajout porte vers l'escape game

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ const app = express();
 const router = express.Router();
 
 const path = __dirname + '/views/';
-const port = 8080;  
+const port = 8080;
 
-router.use(function (req,res,next) {
+router.use(function(req, res, next) {
     console.log('/' + req.method);
     next();
-  });
-  
-  router.get('/', function(req,res){
+});
+
+router.get('/', function(req, res) {
     res.sendFile(path + 'index.html');
-  });
-  
-  router.get('/sharks', function(req,res){
-    res.sendFile(path + 'sharks.html');
-  });
+});
+
+router.get('/house', function(req, res) {
+    res.sendFile(path + 'escape-game.html');
+});
+
 
 app.use(express.static(path));
 app.use('/', router);
 
-app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(port, function() {
+    console.log('Example app listening on port 8080!')
 })
